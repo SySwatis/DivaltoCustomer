e1900cc53cf4c9bf9248842e14287d536a771bc8
Syswatis <syswatis@gmail.com>
2020-06-09 15:41:57 +0200

    initial commit

--- Block/LegalForm.php
+++ Block/LegalForm.php
@@ -24,26 +24,40 @@ class LegalForm extends \Magento\Framework\View\Element\Template
      */
     protected $_helperData;
 
+    /**
+     * Customer serialize
+     *
+     * @var  \Magento\Framework\Serialize\Serializer\Json
+     */
+    protected $_serialize;
+
     public function __construct(
         \Divalto\Customer\Helper\Data $helperData,
-        \Magento\Backend\Block\Template\Context $context,        
+        \Magento\Framework\Serialize\Serializer\Json $serialize,
+        \Magento\Backend\Block\Template\Context $context,     
         array $data = []
     )
     {        
         parent::__construct($context, $data);
         $this->_helperData = $helperData;
+        $this->_serialize = $serialize;
     }
 
     public function getLegalFormHtmlOptions()
     {  
         $options = $this->_helperData->getGeneralConfig('legal_form');
+      
+        
+        if($options == '' || $options == null)
+        return;
+        
+        $unserializedata = $this->_serialize->unserialize($options);
         $html = '';
-        foreach ($options as $key => $value) {
-            # code...
-            $html .= $key.$value;
+        foreach($unserializedata as $key => $row)
+        {
+            $html .= '<option value="'.$row['legal_form_value'].'">'.$row['legal_form_label'].'</option>';
         }
-
-        return '<option>getLegalFormHtmlOptions'. $html .'</option>';
+        return $html;
     }
     
 }
